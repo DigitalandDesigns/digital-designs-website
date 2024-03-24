9a4bf896f3f953bc088f5f06dce1df1ab7677a83
Umman <ummanw@yahoo.com>
2024-03-24 07:21:20 +0500

    astro init with header

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Prompt", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "dd-main-bg": "#111319",
+        "dd-section-bg": "#15171C",
+        "dd-primary": "#DD4242",
+        "dd-grey": "#717171",
+        "dd-card-grey": "#1D1E22",
+      },
+    },
+  },
+  plugins: [],
+};
